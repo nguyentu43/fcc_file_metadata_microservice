3322e397ebd37f534ae934a157e90307e272178f
Glitch (wheat-tub) <none>
2019-06-14 14:09:50 +0000

    🗽🎠 Checkpoint ./server.js:5024683/374 ./package.json:5024683/1989

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ var cors = require('cors');
 // require and use "multer"...
 
 var app = express();
+var multer = require('multer');
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -14,8 +17,13 @@ app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res){
+  const file = req.file;
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  });
 });
 
 app.listen(process.env.PORT || 3000, function () {
